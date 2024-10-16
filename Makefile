c2df91128c054fae9f79e5dd7ef578fb74728f17
William Veith <williamveith@gmail.com>
2024-10-15 21:29:01 -0500

    Adjusted main.go to dynamically determine paths. All features work except certbot. The entrypoint.go file is having an issue. You will have to add dynamic pathing to that to correct that issue

--- Makefile
+++ Makefile
@@ -1,58 +1,58 @@
-# Define the source file and directory
-MODULE_STRAW=straw
-MODULE_APP=appserver
+# Define modules and directories
+MODULE_STRAW = straw
+MODULE_APP = appserver
+SRC_DIR = Buildfiles
+BIN_DIR = bin
+GO_MOD_DIR = $(MODULE_STRAW)/$(SRC_DIR)
 
-SRC_DIR=Buildfiles
-BUILD_FILE_NAME=main.go
-BUILD_PATH=${MODULE_STRAW}/$(SRC_DIR)/$(BUILD_FILE_NAME)
-
-# Define output binary name and output directory
-BINARY_NAME=main
-BIN_DIR=bin
-BUILDER_ROOT_DIR=Buildfiles
-BIN_PATH=$(BIN_DIR)/$(BINARY_NAME)
-MAIN_PATH=$(BUILDER_ROOT_DIR)/$(BIN_PATH)
-
-# Set the working directory to Buildfiles where go.mod is located
-GO_MOD_DIR=$(MODULE_STRAW)/$(SRC_DIR)
+# Define file paths
+BUILD_FILE = main.go
+BUILD_PATH = $(GO_MOD_DIR)/$(BUILD_FILE)
+BIN_PATH = $(GO_MOD_DIR)/$(BIN_DIR)/main
 
 # Load straw/.env
-ifneq (,$(wildcard $(MODULE_STRAW)/.env))
-    include $(MODULE_STRAW)/.env
+ifneq (,$(wildcard $(CURDIR)/$(MODULE_STRAW)/.env))
+    include $(CURDIR)/$(MODULE_STRAW)/.env
     export
 endif
 
 # Build for host system
 compile:
-	cd $(GO_MOD_DIR) && go build -o $(BIN_PATH) $(BUILD_FILE_NAME)
+	GOFLAGS="-mod=mod" go build -o $(BIN_PATH) $(BUILD_PATH)
 
 # Cross-compile for Windows
 compile-windows:
-	cd $(GO_MOD_DIR) && GOOS=windows GOARCH=amd64 go build -o $(BIN_PATH).exe $(BUILD_FILE_NAME)
+	GOOS=windows GOARCH=amd64 GOFLAGS="-mod=mod" go build -o $(BIN_PATH).exe $(BUILD_PATH)
 
 # Cross-compile for macOS
 compile-mac:
-	cd $(GO_MOD_DIR) && GOOS=darwin GOARCH=amd64 go build -o $(BIN_PATH) $(BUILD_FILE_NAME)
+	GOOS=darwin GOARCH=amd64 GOFLAGS="-mod=mod" go build -o $(BIN_PATH) $(BUILD_PATH)
 
 # Cross-compile for Linux
 compile-linux:
-	cd $(GO_MOD_DIR) && GOOS=linux GOARCH=amd64 go build -o $(BIN_PATH) $(BUILD_FILE_NAME)
+	GOOS=linux GOARCH=amd64 GOFLAGS="-mod=mod" go build -o $(BIN_PATH) $(BUILD_PATH)
 
+# Builder and service targets
 builder:
-	$(MAIN_PATH) create-builder
+	$(BIN_PATH) create-builder
 
+nginx:
+	$(BIN_PATH) build-nginx
+	
 cloudflared:
-	$(MAIN_PATH) build-cloudflared
+	$(BIN_PATH) build-cloudflared
 
 certbot:
-	$(MAIN_PATH) build-certbot
+	$(BIN_PATH) build-certbot
 
+# Image signing and verification
 sign:
-	${MAIN_PATH} sign-image $(IMAGE_TAG)
+	$(BIN_PATH) sign-image $(IMAGE_TAG)
 
 verify:
-	${MAIN_PATH} verify-image $(IMAGE_TAG)
+	$(BIN_PATH) verify-image $(IMAGE_TAG)
 
+# Docker compose commands
 start:
 	docker-compose -f $(MODULE_APP)/docker-compose.yml up --build -d
 	docker-compose -f $(MODULE_STRAW)/docker-compose.yml up --build -d
@@ -62,8 +62,10 @@ stop:
 	docker-compose -f $(MODULE_APP)/docker-compose.yml down
 	docker-compose -f $(MODULE_STRAW)/docker-compose.yml down
 
+# Clean up binaries
 clean:
-	cd $(GO_MOD_DIR) && rm -f $(BIN_PATH) $(BIN_PATH).exe
+	rm -f $(BIN_PATH) $(BIN_PATH).exe
 
+# Open the site
 open:
 	open "https://$(HOST_NAME)"
--- straw/Buildfiles/main.go
+++ straw/Buildfiles/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -13,6 +14,12 @@ import (
 func createCustomBuilder() {
 	builderName := os.Getenv("BUILDER_NAME")
 	image := os.Getenv("BUILDER_IMAGE")
+
+	exists := checkBuilderExists(builderName)
+
+	if exists {
+		removeBuilder(builderName)
+	}
 
 	cmd := exec.Command("docker", "buildx", "create",
 		"--name", builderName,
@@ -30,21 +37,74 @@ func createCustomBuilder() {
 	fmt.Println(string(output))
 }
 
+func checkBuilderExists(builderName string) bool {
+	cmd := exec.Command("docker", "buildx", "inspect", builderName)
+	output, err := cmd.CombinedOutput()
+	if err != nil && strings.Contains(string(output), "not found") {
+		return false
+	}
+	return true
+}
+
+func removeBuilder(builderName string) {
+	fmt.Printf("Removing existing builder: %s\n", builderName)
+	cmd := exec.Command("docker", "buildx", "rm", builderName)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		if strings.Contains(string(output), "no builder") {
+			fmt.Println("No existing builder found, skipping removal.")
+		} else {
+			fmt.Println("Failed to remove builder:", err)
+			fmt.Println(string(output))
+		}
+	} else {
+		fmt.Println("Builder removed successfully!")
+	}
+}
+
+func buildNginxImage() {
+	imageTag := "williamveith/nginx:mainline-alpine3.20-slim"
+
+	cmd := exec.Command(
+		"docker", "buildx", "imagetools", "create",
+		"--tag", imageTag,
+		"nginx:mainline-alpine3.20-slim",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Failed to build and push the image:", err)
+		fmt.Println(string(output))
+		return
+	}
+
+	fmt.Println("Image pulled, re-tagged, and pushed successfully!")
+	fmt.Println(string(output))
+
+	signImage(imageTag)
+}
+
 func buildCloudflareImage() {
 	builderName := os.Getenv("BUILDER_NAME")
 	alpineVersion := os.Getenv("CLOUDFLARED_ALPINE_VERSION")
 	cloudflaredVersion := os.Getenv("CLOUDFLARED_VERSION")
 	IMAGE_TAG := fmt.Sprintf("williamveith/cloudflared:%s", cloudflaredVersion)
 
+	projectRoot := findProjectRoot()
+
+	// Construct the paths for Dockerfile and build context
+	dockerfilePath := filepath.Join(projectRoot, "straw", "Dockerfiles", "Dockerfile.cloudflare")
+	buildContext := filepath.Join(projectRoot, "straw", "cloudflared")
+
 	cmd := exec.Command("docker", "buildx", "build",
 		"--platform", "linux/arm64,linux/amd64,linux/386,linux/arm/v7,linux/arm/v6",
 		"--tag", IMAGE_TAG,
 		"--builder", builderName,
 		"--push",
-		"--file", "cloudflared/src/Dockerfile.cloudflare",
+		"--file", dockerfilePath,
 		"--build-arg", fmt.Sprintf("CLOUDFLARED_ALPINE_VERSION=%s", alpineVersion),
 		"--build-arg", fmt.Sprintf("CLOUDFLARED_VERSION=%s", cloudflaredVersion),
-		".")
+		buildContext)
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -63,14 +123,19 @@ func buildCertbotImage() {
 	certbotImageVersion := os.Getenv("CERTBOT_IMAGE_VERSION")
 	IMAGE_TAG := fmt.Sprintf("williamveith/certbot:%s", certbotImageVersion)
 
+	projectRoot := findProjectRoot()
+
+	dockerfilePath := filepath.Join(projectRoot, "straw", "Dockerfiles", "Dockerfile.certbot")
+	buildContext := filepath.Join(projectRoot, "straw", "certbot")
+
 	cmd := exec.Command("docker", "buildx", "build",
 		"--platform", "linux/amd64,linux/arm/v6,linux/arm64",
 		"--build-arg", fmt.Sprintf("CERTBOT_GOLANG_VERSION=%s", certbotGolangVersion),
 		"--build-arg", fmt.Sprintf("CERTBOT_IMAGE_VERSION=%s", certbotImageVersion),
 		"--tag", IMAGE_TAG,
-		"--file", "Dockerfiles/Dockerfile.certbot",
+		"--file", dockerfilePath,
 		"--push",
-		".")
+		buildContext)
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -86,16 +151,15 @@ func buildCertbotImage() {
 
 func signImage(IMAGE_TAG string) {
 	fmt.Println("Signing Image...")
-	KEY_PATH := os.Getenv("KEY_PATH")
-	if KEY_PATH == "" {
-		KEY_PATH = "Buildfiles"
-	}
+
+	projectRoot := findProjectRoot()
+	keyPath := filepath.Join(projectRoot, "straw", "Buildfiles")
 
 	os.Setenv("COSIGN_PASSWORD", "")
 	os.Setenv("COSIGN_EXPERIMENTAL", "1")
 
 	cmd := exec.Command("cosign", "sign",
-		"--key", fmt.Sprintf("%s/cosign.key", KEY_PATH),
+		"--key", fmt.Sprintf("%s/cosign.key", keyPath),
 		IMAGE_TAG)
 
 	cmd.Stdin = strings.NewReader("y\n")
@@ -112,9 +176,11 @@ func signImage(IMAGE_TAG string) {
 }
 
 func verifyImage(IMAGE_TAG string) {
-	KEY_PATH := "Buildfiles"
+	projectRoot := findProjectRoot()
+	keyPath := filepath.Join(projectRoot, "straw", "Buildfiles")
+
 	cmd := exec.Command("cosign", "verify",
-		"--key", fmt.Sprintf("%s/cosign.pub", KEY_PATH),
+		"--key", fmt.Sprintf("%s/cosign.pub", keyPath),
 		IMAGE_TAG)
 
 	output, err := cmd.CombinedOutput()
@@ -128,21 +194,60 @@ func verifyImage(IMAGE_TAG string) {
 	fmt.Println(string(output))
 }
 
+func loadEnv() {
+	projectRoot := findProjectRoot()
+
+	envPath := filepath.Join(projectRoot, "straw", ".env")
+
+	if err := godotenv.Load(envPath); err != nil {
+		log.Printf("Error loading .env file: %v", err)
+	} else {
+		fmt.Println(".env file loaded successfully from:", envPath)
+	}
+}
+
+func findProjectRoot(startDir ...string) string {
+	var dir string
+	if len(startDir) == 0 || startDir[0] == "" {
+		wd, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir = wd
+	} else {
+		dir = startDir[0]
+	}
+
+	currentDir := dir
+	for {
+		envPath := filepath.Join(currentDir, "straw", ".env")
+		if _, err := os.Stat(envPath); err == nil {
+			return currentDir
+		}
+
+		parentDir := filepath.Dir(currentDir)
+		if parentDir == currentDir {
+			log.Fatal(".env file not found")
+		}
+		currentDir = parentDir
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a command: create-builder, build-cloudflared, or build-certbot")
 		return
 	}
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+	loadEnv()
 
 	switch os.Args[1] {
 	case "create-builder":
 		fmt.Println("Creating builder...")
 		createCustomBuilder()
+	case "build-nginx":
+		fmt.Println("Building nginx...")
+		buildNginxImage()
 	case "build-cloudflared":
 		fmt.Println("Building cloudflared...")
 		buildCloudflareImage()
